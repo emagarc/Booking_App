9039b5f8110e8264b60489b46b54f6afcd0eb431
Emanuel Garcia <wemagw@gmail.com>
2023-05-31 17:17:28 -0300

    first steps

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface LogoProps {
+
+}
+
+const Logo: React.FC<LogoProps> = ({
+
+}) => {
+    const router = useRouter();
+  return (
+    <Image
+        alt="Logo"
+        className="hidden md:block cursor-pointer"
+        height="100"
+        width="100"
+        src="/images/logo.png"
+    />
+  )
+}
+
+export default Logo;
