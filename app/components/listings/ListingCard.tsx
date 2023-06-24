2064805a76957c6613c580df358bb0a9f1efb2c6
Emanuel Garcia <wemagw@gmail.com>
2023-06-23 22:59:47 -0300

    onAction fixed bug

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -14,7 +14,7 @@ import Button from "../Button";
 interface ListingCardProps {
     data: SafeListing;
     reservation?: SafeReservation;
-    onAction: (id: string) => void;
+    onAction?: (id: string) => void;
     disabled?: boolean;
     actionLabel?: string;
     actionId?: string;
